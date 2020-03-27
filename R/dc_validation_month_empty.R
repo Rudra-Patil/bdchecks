4ec911b1a6667766d130f557939d37bd70a68c97
Rudra Patil <42993095+Rudra-Patil@users.noreply.github.com>
2020-03-27 15:23:09 +0530

    Data check feature to detect an empty month input

---
+++ R/dc_validation_month_empty.R
@@ -0,0 +1,7 @@
+
+dc_validation_month_empty <- function(TARGET = NULL) {
+  result <- TARGET %>%
+    gsub(" ", "", .) # Remove possible spaces
+  result <- !is.na(result) & result != "" # Check if not empty
+  return(result)
+}
