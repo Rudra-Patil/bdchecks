261f68e9a39928e94ecbcaa9f9b1662f3fdd05d0
Rudra Patil <42993095+Rudra-Patil@users.noreply.github.com>
2020-03-27 20:56:02 +0530

    Added roxygen comments

--- R/dc_validation_month_empty.R
+++ R/dc_validation_month_empty.R
@@ -1,4 +1,9 @@
 
+#' @rdname dc_validation_month_empty
+#'
+#' @param TARGET a vector of month information. It must be non empty entry to pass the function.
+#'
+#'
 dc_validation_month_empty <- function(TARGET = NULL) {
   result <- TARGET %>%
     gsub(" ", "", .) # Remove possible spaces
